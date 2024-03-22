6ff2e0634208991d54735431456c028ff46d69d5
Will Tham <tham.will00@gmail.com>
2024-03-21 23:30:00 -0700

    GET for all recipes done

--- recipe/src/recipe/recipe.service.ts
+++ recipe/src/recipe/recipe.service.ts
@@ -1,11 +1,31 @@
-import { Body, Injectable, Post } from '@nestjs/common';
-import { CreateRecipeDto } from './dto/create--recipe.dto';
-import { UpdateRecipeDto } from './dto/update--recipe.dto';
+import { Injectable } from '@nestjs/common';
+import { CreateRecipeDto } from './dto/create-recipe.dto';
+import { UpdateRecipeDto } from './dto/update-recipe.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class RecipesService {
+export class RecipeService {
   constructor(private readonly prisma: PrismaService) {}
 
-  //  rest of the code
+  create(createRecipeDto: CreateRecipeDto) {
+    return 'This action adds a new recipe';
+  }
+
+  // findAll method utilizes Prismas findMany method to retrieve all recipes from the database
+  // await is not needed here because async function implicitly returns a promise
+  async findAll() {
+    return this.prisma.recipe.findMany();
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} recipe`;
+  }
+
+  update(id: number, updateRecipeDto: UpdateRecipeDto) {
+    return `This action updates a #${id} recipe`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} recipe`;
+  }
 }
